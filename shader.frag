7605f1ef06957a432c45311158493dbc9acc0c5c
Christoph Helma <helma@in-silico.ch>
2017-07-21 13:53:27 +0200

    shader reload, shader arrays

--- shader.frag
+++ shader.frag
@@ -5,36 +5,26 @@ precision mediump float;
 
 uniform vec2 resolution;
 uniform float time;
-uniform float m;
-//uniform sampler2D backbuffer;
 
-uniform sampler2D i0;
-uniform sampler2D i1;
-uniform sampler2D i2;
-uniform sampler2D i3;
-uniform float i0ratio;
-uniform float i1ratio;
-uniform float i2ratio;
-uniform float i3ratio;
-
-uniform float p0;
-uniform float p1;
-uniform float p3;
-uniform float p4;
+uniform sampler2D images[4];
+uniform float ratios[4];
+uniform float params[32];
 
 out vec4 fragColor;
+
 float random (in float x) {
-    return fract(sin(x)*43758.5453123)-0.5;
+  return fract(sin(x)*43758.5453123)-0.5;
+}
+
+vec4 image(int i) {
+	vec2 st = gl_FragCoord.xy/resolution.xy;
+  st.x *= ratios[i];
+  st.x += (1-ratios[i])*0.5;
+  return texture2D(images[i],st);
 }
 
 void main (void) {
-	vec2 st = gl_FragCoord.xy/resolution.xy;
-  vec4 img1 = texture2D(i0,st*vec2(i0ratio,1.));
-  vec4 img2 = texture2D(i1,st*vec2(i1ratio,1.));
-  vec4 img3 = texture2D(i2,st*vec2(i2ratio,1.));
-  vec4 img4 = texture2D(i3,st*vec2(i3ratio,1.));
-	img1 = mix(img1,img2,sin(time));
-	img2 = mix(img3,img4,cos(time));
-	fragColor = mix(img1,img2,p0);
-	//gl_FragColor = i1;
+	vec4 img1 = mix(image(0),image(1),sin(time));
+	vec4 img2 = mix(image(2),image(3),cos(time));
+  fragColor = mix(img1,img2,params[1]);
 }
--- sv.c
+++ sv.c
@@ -4,9 +4,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <pthread.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 #define STB_IMAGE_IMPLEMENTATION
-#include "std/stb_image.h"
+#include "stb_image.h"
 
 int width = 1920;
 int height = 1080;
@@ -17,7 +20,9 @@ GLuint shaderProgram;
 
 typedef struct Shad {
   GLuint id;
-  char path[40];
+  char vertex[40];
+  char fragment[40];
+  time_t mtime;
   int new;
 } Shader;
 
@@ -33,12 +38,12 @@ typedef struct Img {
 Image images[4];
 
 typedef struct Param {
-  char name[3];
+  //char name[3];
   float value;
   int new;
 } Parameter;
 
-Parameter parameters[16];
+Parameter parameters[32];
 
 static void error_callback(int error, const char* description) { fputs(description, stderr); }
 
@@ -47,14 +52,8 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 }
 
 const char * readShader(char * path) {
-  fputs(path,stderr);
-  fputs("\n",stderr);
   FILE *file = fopen(path, "r");
-  if (!file) {
-    fputs("Cannot read ",stderr);
-    fputs(path,stderr);
-    exit(EXIT_FAILURE);
-  }
+  if (!file) { fprintf(stderr,"Cannot read %s\n",path); }
   char source[2000];
   int res = fread(source,1,2000-1,file);
   source[res] = 0;
@@ -72,9 +71,7 @@ GLuint compileShader(const char * source, GLenum type) {
   if (!success) {
     char infoLog[512];
     glGetShaderInfoLog(sh, 512, NULL, infoLog);
-    fputs("Shader compilation failed:\n",stderr);
-    fputs(infoLog,stderr);
-    exit(EXIT_FAILURE);
+    fprintf(stderr,"Shader compilation failed: %s\n",infoLog);
   }
   else { return sh; }
 }
@@ -87,18 +84,27 @@ GLuint linkShader(GLuint vertex, GLuint fragment) {
   glAttachShader(shader.id, fragment);
   glLinkProgram(shader.id);
   int success;
-  char infoLog[512];
   glGetProgramiv(shader.id, GL_LINK_STATUS, &success);
   if (!success) {
+    char infoLog[512];
     glGetProgramInfoLog(shader.id, 512, NULL, infoLog);
-    fputs("Shader linking failed:\n",stderr);
-    fputs(infoLog,stderr);
+    fprintf(stderr,"Shader linking failed: %s\n",infoLog);
   }
   glDetachShader(shader.id, vertex); 
   glDetachShader(shader.id, fragment); 
   glDeleteShader(vertex);
   glDeleteShader(fragment);
 }
+
+void createShader() {
+  vertex = compileShader(readShader(shader.vertex), GL_VERTEX_SHADER);
+  fragment = compileShader(readShader(shader.fragment),GL_FRAGMENT_SHADER);
+  linkShader(vertex,fragment);
+  struct stat file_stat;
+  stat(shader.fragment, &file_stat);
+  shader.mtime = file_stat.st_mtime;
+  glUseProgram(shader.id);
+};
 
 void createWindow() {
   glfwInit();
@@ -109,14 +115,6 @@ void createWindow() {
   glfwSetErrorCallback(error_callback);
   glewInit();
 }
-
-void createShader(char *vert, char *frag) {
-  vertex = compileShader(readShader(vert), GL_VERTEX_SHADER);
-  fragment = compileShader(readShader(frag),GL_FRAGMENT_SHADER);
-  linkShader(vertex,fragment);
-  printf("%i\n",shader.id);
-  glUseProgram(shader.id);
-};
 
 void uniform1f(char * var, float f) {
   int v = glGetUniformLocation(shader.id, var);
@@ -129,13 +127,13 @@ void uniform2f(char * var, float f0, float f1) {
 }
 
 void imageUniforms(int i) {
-  Image image = images[i];
-  char name[2];
-  sprintf(name,"i%i",i);
-  glUniform1i(glGetUniformLocation(shader.id, name), i);
-  char ratioName[7];
-  sprintf(ratioName,"%sratio",name);
-  uniform1f(ratioName,images[i].ratio);
+  //Image image = images[i];
+  //char name[2];
+  //sprintf(name,"i%i",i);
+  glUniform1i(glGetUniformLocation(shader.id, "images")+i,i);
+  //char ratioName[7];
+  //sprintf(ratioName,"%sratio",name);
+  glUniform1f(glGetUniformLocation(shader.id, "ratios")+i,images[i].ratio);
 }
 
 void readImage(int i) {
@@ -160,35 +158,6 @@ void readImage(int i) {
   stbi_image_free(pixels);
 }
 
-void *readStdin() {
-
-  while (1) {
-    char * str;
-    size_t l = 80;
-    getline(&str,&l,stdin);
-    char *n = strtok(str," ");
-
-    if (n[0] == 'i') {
-      int i = n[1]-'0';
-      strncpy(images[i].path, strtok(NULL,"\n"),40);
-      images[i].new = 1;
-    }
-    else if (n[0] == 'f') {
-      strncpy(shader.path, strtok(NULL,"\n"),40);
-      shader.new = 1;
-    }
-    else if (n[0] == 'p') {
-      int i = n[1]-'0';
-      strncpy(parameters[i].name,n,3);
-      parameters[i].value = atof(strtok (NULL,"\n"));
-      parameters[i].new = 1;
-    }
-    else if (n[0] == 'q') {
-      glfwSetWindowShouldClose(window, GL_TRUE);
-    }
-  }
-}
-
 void updateImages(int force) {
   for (int i = 0; i<4; i++) {
     if (images[i].new || force) {
@@ -199,48 +168,87 @@ void updateImages(int force) {
   }
 }
 
-void updateUniforms(int force) {
-  for (int i = 0; i<16; i++) {
+void updateParams(int force) {
+  for (int i = 0; i<32; i++) {
     if (parameters[i].new || force) {
-      uniform1f(parameters[i].name,parameters[i].value);
+      glUniform1f(glGetUniformLocation(shader.id, "params")+i, parameters[i].value);
       parameters[i].new = 0;
     }
+  }
+}
+
+void *readStdin() {
+  while (1) {
+    char * str;
+    size_t l = 80;
+    getline(&str,&l,stdin);
+    char *n = strtok(str," ");
+    if (n[0] == 'i') {
+      int i = atoi(strtok(NULL," "));
+      strncpy(images[i].path, strtok(NULL,"\n"),40);
+      images[i].new = 1;
+    }
+    else if (n[0] == 'f') {
+      strncpy(shader.fragment, strtok(NULL,"\n"),40);
+      shader.new = 1;
+    }
+    else if (n[0] == 'p') {
+      int i = atoi(strtok(NULL," "));
+      parameters[i].value = atof(strtok (NULL,"\n"));
+      parameters[i].new = 1;
+    }
+    else if (n[0] == 'q') {
+      glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+  }
+}
+
+void *watchShader() {
+  while (1) {
+    sleep(0.2);
+    struct stat file_stat;
+    stat(shader.fragment, &file_stat);
+    if (file_stat.st_mtime > shader.mtime) shader.new = 1; 
   }
 }
 
 int main(int argc, char **argv) {
 
   createWindow();
-  createShader("shader.vert","shader.frag");
+  strncpy(shader.vertex,"shader.vert",40);
+  strncpy(shader.fragment,"shader.frag",40);
+  shader.new = 1;
   unsigned int VAO;
   glGenVertexArrays(1, &VAO);
   glBindVertexArray(VAO);
-  for (int i = 0; i<4; i++) {
-    strncpy(images[i].path, argv[i+1],40);
-    images[i].new = 1;
+  for (int i = 1; i<argc; i++) {
+    strncpy(images[i-1].path, argv[i],40);
+    images[i-1].new = 1;
   }
-  uniform2f("resolution", width,height);
   
-  pthread_t tid;
-  pthread_create(&tid, NULL, readStdin, NULL);
+  pthread_t stdin_t;
+  pthread_create(&stdin_t, NULL, readStdin, NULL);
+  pthread_t frag_t;
+  pthread_create(&frag_t, NULL, watchShader, NULL);
 
   while (!glfwWindowShouldClose(window)) {
-    uniform1f("time",glfwGetTime());
+    glUniform1f(glGetUniformLocation(shader.id, "time"),glfwGetTime());
     if (shader.new) {
-      createShader("shader.vert",shader.path);
+      createShader();
       uniform2f("resolution", width,height); // important!!
       for (int i = 0; i<4; i++) { imageUniforms(i); }
-      updateUniforms(1);
+      updateParams(1);
       shader.new = 0;
     }
     updateImages(0);
-    updateUniforms(0);
+    updateParams(0);
     glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
 
-  pthread_cancel(tid);
+  pthread_cancel(stdin_t);
+  pthread_cancel(frag_t);
   glfwDestroyWindow(window);
   glfwTerminate();
  
